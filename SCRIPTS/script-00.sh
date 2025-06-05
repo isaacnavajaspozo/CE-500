5e91e4e05fe6f4b2ea5c3cc4ccf107c11a8ddb30
Isaac Navajas Pozo <51831724+isaacnavajaspozo@users.noreply.github.com>
2025-06-05 20:42:59 +0200

    Update script-00.sh

--- SCRIPTS/script-00.sh
+++ SCRIPTS/script-00.sh
@@ -320,5 +320,5 @@ EOF
 echo -e "# Deshabilitamos IPv6\nnet.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 sysctl -p
 
-echo -e "\e[31m⚠️ Recuerda cambiar el host en /etc/hosts por el mismo que acabas de agregar en /etc/hostname\e[0m"
+echo -e "\e[31m ⚠ Recuerda cambiar el host en /etc/hosts por el mismo que acabas de agregar en /etc/hostname\e[0m"
 echo "¡Listo! Los paquetes se instalaron y la configuración está completa."
