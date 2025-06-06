ee292a593e5f667651de2ce36a9cdf68857d5229
Isaac Navajas Pozo <51831724+isaacnavajaspozo@users.noreply.github.com>
2025-06-06 12:57:36 +0200

    Update script-00.sh

--- SCRIPTS/script-00.sh
+++ SCRIPTS/script-00.sh
@@ -93,7 +93,7 @@ if ! command -v nmap &>/dev/null; then
   fi
 fi
 
-echo "[*] Esta función realiza un escaneo de vulnerabilidades sobre la IP especificada."
+echo "[*] Este comando realiza un escaneo de vulnerabilidades sobre la IP especificada."
 read -rp "Introduce la IP a escanear: " ip
 
 if [[ -z "$ip" ]]; then
